c2c3aa4417690d00cb6849b54a36c1b31c15e1b1
Louie O Hagan <louieohagan101@gmail.com>
2020-08-31 17:05:42 +0100

    Adding build

---
+++ build/precache-manifest.fe088c4bd6b1c39c0440ab61f48af248.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71831a32aa55c3d82b797839098602fd",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "509908b9289e9576b19a",
+    "url": "/monsters-rolodex/static/css/main.1dfda926.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "509908b9289e9576b19a",
+    "url": "/monsters-rolodex/static/js/main.e7070f70.chunk.js"
+  },
+  {
+    "revision": "6062e4a584c29c6e0372",
+    "url": "/monsters-rolodex/static/js/runtime-main.e3d252a9.js"
+  }
+]);
